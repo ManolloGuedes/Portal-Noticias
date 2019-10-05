7c1b668f65ca123f006d95004ca667948f7678d5
Herlon Manollo Candido Guedes <manollo.guedes11@gmail.com>
2019-10-04 20:15:28 -0400

    [Refactor] - Using consign to map routes
    Using consign is not necessary to map route by route at app.js.
    The method include() indicates when are the routes, after that, into() add this routes in our app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 let app = require('./config/server');
 
-let rotaHome = require('./app/routes/home');
-rotaHome(app);
-
-let rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
-
-let rotaFormularioInclusaoNoticias = require('./app/routes/formularioInclusaoNoticia');
-rotaFormularioInclusaoNoticias(app);
-
 app.listen(3000, () => {
     console.log('Server is running');
 })
