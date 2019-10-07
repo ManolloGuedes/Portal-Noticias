b031cbe7ca527b29a3b9bc229edaf7ea6d67f293
Herlon Manollo Candido Guedes <manollo.guedes11@gmail.com>
2019-10-07 15:56:02 -0400

    [Refactor] - Merge between routes noticia and noticias

--- src/app/routes/noticias.js
+++ src/app/routes/noticias.js
@@ -10,5 +10,16 @@ module.exports = (application) => {
             res.render('noticias/noticias', {noticias : result});
         });
 
-    })
+    });
+
+    application.get('/noticia', (req, res) => {
+
+        let connection = application.config.dataBaseConnection();
+        let noticiaDAO = new application.app.models.NoticiaDAO(connection);
+
+        noticiaDAO.getNoticia((err, result) => {
+            res.render('noticias/noticia', {noticia : result});
+        });
+
+    });
 };
