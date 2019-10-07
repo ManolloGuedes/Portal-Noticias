e203003b240cc1ccc593b240a75261673c7dc035
Herlon Manollo Candido Guedes <manollo.guedes11@gmail.com>
2019-10-07 15:49:12 -0400

    [Add] - Validating form using express-validator

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -1,10 +1,25 @@
 module.exports = (application) => {
     application.get('/formulario_inclusao_noticia', (req, res) => {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {error : {}, noticia : {}});
     });
 
     application.post('/noticias/salvar', (req, res) => {
         let noticia = req.body;
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('nomeautor', 'Nome do autor é obrigatório').notEmpty();
+        req.assert('datanoticia', 'Data da notícia é obrigatório').notEmpty()
+            .isDate({format : 'YYYY-MM-DD'});
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        let error = req.validationErrors();
+
+        if(error) {
+            res.render('admin/form_add_noticia', {error : error, noticia : noticia});
+            return;
+        }
 
         let connection = application.config.dataBaseConnection();
         let noticiaDAO = new application.app.models.NoticiaDAO(connection);
